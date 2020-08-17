1da5894ab00fd1447d7dcb88a55a43c886a79154
joo98 <slew61@naver.com>
2020-08-17 15:54:50 +0900

    search 화면 추가 및 메인에서 연결완료

--- app/src/main/java/com/teamide/idecomobility/MainActivity.java
+++ app/src/main/java/com/teamide/idecomobility/MainActivity.java
@@ -2,7 +2,12 @@ package com.teamide.idecomobility;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText startText = (EditText) findViewById(R.id.editText1);
+
+
+        startText.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getApplicationContext(),SearchActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
     }
+
 }
---
+++ app/src/main/java/com/teamide/idecomobility/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.teamide.idecomobility;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import java.util.Locale;
+
+public class SearchActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+    }
+}
