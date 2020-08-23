b71686629d8763ce3576e0bbaa7e5ef722368172
joo98 <slew61@naver.com>
2020-08-23 23:56:12 +0900

    1차완료

--- app/src/main/java/com/teamide/idecomobility/BusTime.java
+++ app/src/main/java/com/teamide/idecomobility/BusTime.java
@@ -14,17 +14,16 @@ public class BusTime {
     Element element;
     String busId;
 
-    BusTime(String s)
-    {
+    BusTime(String s) {
         this.busId = s;
         busXml();
     }
 
-    public void busXml(){
+    public void busXml() {
         try {
             String url = "http://ws.bus.go.kr/api/rest/arrive/getLowArrInfoByStId?"
                     + "ServiceKey=ESol2fpbzP%2F%2BPJfShFAU%2FaMbhqzepxntyCIskM2bqyY0dWWU4Sd1w0VH0JpBnWCdBUd79%2BLI34mOXFs0UjrMJA%3D%3D"
-                    + "&stId="+busId+"";
+                    + "&stId=" + busId + "";
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(url);
@@ -33,42 +32,34 @@ public class BusTime {
             NodeList nList = doc.getElementsByTagName("itemList");
             Node nNode = nList.item(0);
             element = (Element) nNode;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static String getTagValue(String tag, Element element){
+    public static String getTagValue(String tag, Element element) {
         NodeList nList = element.getElementsByTagName(tag).item(0).getChildNodes();
         Node nValue = (Node) nList.item(0);
-        if (nValue == null){
+        if (nValue == null) {
             return null;
         }
         return nValue.getNodeValue();
     }
 
-    public ArrayList<String> getData()
-    {
+    public ArrayList<String> getData() {
         ArrayList<String> data = new ArrayList<String>();
-        String busstop = getTagValue("stNm",element); // 버스정류장 이름 가져오기
-        String time = getTagValue("exps1",element); // 남은시간 블러오기 (초)
-        Integer i = Integer.parseInt(time)/60;
-<<<<<<< HEAD
+        String busstop = getTagValue("stNm", element); // 버스정류장 이름 가져오기
+        String time = getTagValue("exps1", element); // 남은시간 블러오기 (초)
+        Integer i = Integer.parseInt(time) / 60;
+
         String bustm = Integer.toString(i); // 남은시간 블러오기 (분)
         data.add(busstop);
-=======
-        String bustm = Integer.toString(i); // 남은시간 블러오기 (분) //정수 추가 부분 끝
-        data.add(busstop);
         data.add(time);
->>>>>>> origin/master
         data.add(bustm);
 
         return data;
 
     }
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/master
+
--- app/src/main/java/com/teamide/idecomobility/RouteResultActivity.java
+++ app/src/main/java/com/teamide/idecomobility/RouteResultActivity.java
@@ -21,19 +21,16 @@ import org.json.JSONException;
 import java.util.ArrayList;
 
 public class RouteResultActivity extends Activity {
-    public String busStationNm = "";
-    public String busTm = "";
-    public String subwayId = "";
 
-    public String localId = "";
-    public String startId ="";
     public String sLatitude = "37.6281126";
     public String sLongitude = "127.09045680000001";
     public String eLatitude = "37.654527";
     public String eLongitude = "127.060551";
 
+
     ArrayList<direction_data> movieDataList;
-    //AIzaSyDX8dROz3L3e1jMQ7tUE1CC094guImPlo8"
+    ArrayList<String> searchPathData = new ArrayList<String>();
+    ArrayList<String> busData = new ArrayList<String>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,19 +44,48 @@ public class RouteResultActivity extends Activity {
         sLongitude = Double.toString(infoAddress.getStartAddress().getLongitude());
         eLatitude = Double.toString(infoAddress.getEndAddress().getLatitude());
         eLongitude = Double.toString(infoAddress.getEndAddress().getLongitude());
-        Log.d("ad",sLatitude+"    "+sLongitude+"   "+eLatitude+"  "+eLongitude);
 
-        searchPath(infoAddress.getStartAddress(),infoAddress.getEndAddress());//길찾기 실행
-        searchBusID(startId);
 
-        BusTime bustm = new BusTime(localId);
-        bustm.getData();
+        ODsayService odsayService = ODsayService.init(RouteResultActivity.this, "nFVGyVxSTk6opjbmKKPCTDaEfNWyidhvs1HbmTtAf6U");
 
-        busStationNm = (bustm.getData()).get(0);
-        busTm = (bustm.getData()).get(1);
+        OnResultCallbackListener onResultCallbackListener = new OnResultCallbackListener() {
+            // 호출 성공 시 실행
+            @Override
+            public void onSuccess(ODsayData odsayData, API api) {
+                try {
+                    // API Value 는 API 호출 메소드 명을 따라갑니다.
+                    if (api == API.SEARCH_PUB_TRANS_PATH) {//여기가 오디세이 데이터 처리하는 함수
+                        String searchType = odsayData.getJson().getJSONObject("result").getString("searchType");
+                        JSONArray path =  odsayData.getJson().getJSONObject("result").getJSONArray("path");
+                        String totalTime = path.getJSONObject(0).getJSONObject("info").getString("totalTime");
+                        String payment = path.getJSONObject(0).getJSONObject("info").getString("payment");
+                        String stationId = path.getJSONObject(0).getJSONArray("subPath").getJSONObject(1).getJSONObject("passStopList").getJSONArray("stations").getJSONObject(0).toString();
+                        Log.d("ad","오딧세이 호출 searchType :"+ searchType+"총 걸리는 시간:"+totalTime+"총 요금:"+payment+"정류장ID"+stationId);
+                        searchPathData.add(searchType);
+                        searchPathData.add(totalTime);
+                        searchPathData.add(payment);
+                        searchPathData.add(stationId);
 
-        Log.d("ad",busStationNm);
-        Log.d("ad",busTm);
+                        Log.d("ad",searchPathData.toString());
+                        Log.d("ad",busData.toString());
+
+                    }
+                }catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 호출 실패 시 실행
+            @Override
+            public void onError(int i, String s, API api) {
+                if (api == API.SEARCH_PUB_TRANS_PATH) {
+                    Log.d("ad","오딧세이 Can't read data");
+                    Log.d("ad","에러코드"+s);
+                }
+            }
+        };
+
+        odsayService.requestSearchPubTransPath(sLongitude,sLatitude,eLongitude,eLatitude,"0","0","0",onResultCallbackListener);
+
 
         TextView startTextView = findViewById(R.id.start);
         TextView endTextView = findViewById(R.id.end);
@@ -67,7 +93,7 @@ public class RouteResultActivity extends Activity {
         startTextView.setText(infoAddress.getStartAddress().getMainAdress());
         endTextView.setText(infoAddress.getEndAddress().getMainAdress());
 
-        this.InitializeMovieData(busStationNm,busTm,subwayId);//->72
+        this.InitializeMovieData();
 
         ListView listView = (ListView)findViewById(R.id.listView);
         final directionAdapter myAdapter = new directionAdapter(this,movieDataList);
@@ -82,87 +108,17 @@ public class RouteResultActivity extends Activity {
                         Toast.LENGTH_LONG).show();
             }
         });
+
     }
 
-    public void InitializeMovieData(String a, String b,String c) //data받아오기
+    public void InitializeMovieData() //data받아오기
     {
         movieDataList = new ArrayList<direction_data>();
         //이부분을 case문으로 처리
         movieDataList.add(new direction_data("출발", "출발지",null, null, null));
-        movieDataList.add(new direction_data("버스", a,"362",b,"분 뒤 도착"));
+        movieDataList.add(new direction_data("버스", "서울여대 후문 정류소","362","3","분 뒤 도착"));
         movieDataList.add(new direction_data("도보", "도보로 420m 이동","태릉입구역 하차후 6번 출구 엘리베이터",null,null));
-        movieDataList.add(new direction_data("지하철", c+"역","4-1, 6-1, 8-1","3","분 뒤 도착"));
+        movieDataList.add(new direction_data("지하철", "태릉입구역","4-1, 6-1, 8-1","2","분 뒤 도착"));
         movieDataList.add(new direction_data("도착", "도착지",null,null,null));
-    }
-    public void searchPath(SearchAddress startAddress,SearchAddress endAddress)//오딧세이 경로 찾기 함수
-    {
-        ODsayService odsayService = ODsayService.init(this, "nFVGyVxSTk6opjbmKKPCTDaEfNWyidhvs1HbmTtAf6U");
-        odsayService.setReadTimeout(5000);
-        odsayService.setConnectionTimeout(5000);
-
-        // 콜백 함수 구현
-        OnResultCallbackListener onResultCallbackListener = new OnResultCallbackListener() {
-            // 호출 성공 시 실행
-            @Override
-            public void onSuccess(ODsayData odsayData, API api) {
-                try {
-                    // API Value 는 API 호출 메소드 명을 따라갑니다.
-                    if (api == API.SEARCH_PUB_TRANS_PATH) {//여기가 오디세이 데이터 처리하는 함수
-                        String searchType = odsayData.getJson().getJSONObject("result").getString("searchType");
-                        JSONArray path = odsayData.getJson().getJSONObject("result").getJSONArray("path");
-                        String totalTime = path.getJSONObject(0).getJSONObject("info").getString("totalTime");
-                        String payment = path.getJSONObject(0).getJSONObject("info").getString("payment");
-                        Integer busstId = path.getJSONObject(0).getJSONObject("subPath").getInt("startID");
-                        startId = Integer.toString(busstId);
-                        subwayId = path.getJSONObject(0).getJSONObject("subPath").getJSONObject("passStopList").getJSONObject("stations").getString("stationID");
-
-                        Log.d("ad", "오딧세이 호출 searchType :" + searchType + "총 걸리는 시간:" + totalTime + "총 요금:" + payment);
-                    }
-                }catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            // 호출 실패 시 실행
-            @Override
-            public void onError(int i, String s, API api) {
-                if (api == API.SEARCH_PUB_TRANS_PATH) {
-                    Log.d("ad","오딧세이1 Can't read data");
-                    Log.d("ad","에러코드"+s);
-                }
-            }
-        };
-        odsayService.requestSearchPubTransPath(sLongitude,sLatitude,eLongitude,eLatitude,"0","0","0",onResultCallbackListener);
-    }
-    public void searchBusID(String busid)//오딧세이 경로 찾기 함수
-    {
-        ODsayService odsayService = ODsayService.init(this, "nFVGyVxSTk6opjbmKKPCTDaEfNWyidhvs1HbmTtAf6U");
-        odsayService.setReadTimeout(5000);
-        odsayService.setConnectionTimeout(5000);
-
-        // 콜백 함수 구현
-        OnResultCallbackListener onResultCallbackListener = new OnResultCallbackListener() {
-            // 호출 성공 시 실행
-            @Override
-            public void onSuccess(ODsayData odsayData, API api) {
-                try {
-                    // API Value 는 API 호출 메소드 명을 따라갑니다.
-                    if (api == API.BUS_STATION_INFO) {//여기가 오디세이 데이터 처리하는 함수
-                        Integer lcid = odsayData.getJson().getJSONObject("result").getInt("localStationID");
-                        localId = Integer.toString(lcid);
-                    }
-                }catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            // 호출 실패 시 실행
-            @Override
-            public void onError(int i, String s, API api) {
-                if (api == API.BUS_STATION_INFO) {
-                    Log.d("ad","오딧세이2 Can't read data");
-                    Log.d("ad","에러코드"+s);
-                }
-            }
-        };
-        odsayService.requestSearchPubTransPath(sLongitude,sLatitude,eLongitude,eLatitude,"0","0","0",onResultCallbackListener);
     }
 }
