6632d7a214f22b15c3519e151d7a9f3dd321f840
Jeongsu <in5451@naver.com>
2020-09-12 10:45:26 +0900

    가상데이터출력완료

--- app/src/main/java/com/teamide/idecomobility/TransData.java
+++ app/src/main/java/com/teamide/idecomobility/TransData.java
@@ -1,15 +1,19 @@
 package com.teamide.idecomobility;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 public class TransData {
     String jsonbus;
     String jsonsubway;
     String jsonimg;
 
-    void jsonBus(){
+    public TransData() {
+        jsonBus();
+        jsonSubway();
+        jsonImg();
+    }
+
+    String jsonBus(){
         JSONObject inner = new JSONObject();
         inner.put("stNm","서울 여대 후문 정류소");
         inner.put("rtNm","7738");
@@ -20,9 +24,9 @@ public class TransData {
 
         jsonbus = outer.toJSONString();
 
-        System.out.println(jsonbus);
+        return jsonbus;
     }
-    void jsonSubway(){
+    String jsonSubway(){
         JSONObject inner = new JSONObject();
         inner.put("statnNm","태릉입구");
         inner.put("barvlDt_sec","420");
@@ -32,9 +36,9 @@ public class TransData {
 
         jsonsubway = outer.toJSONString();
 
-        System.out.println(jsonsubway);
+        return jsonsubway;
     }
-    void jsonImg(){
+    String jsonImg(){
         JSONObject inner = new JSONObject();
         inner.put("storedName","http://indoormap.seoul.go.kr/app/openapi/seoulcity/handicapPOIInfo/84/stream");
 
@@ -43,6 +47,6 @@ public class TransData {
 
         jsonimg = outer.toJSONString();
 
-        System.out.println(jsonsubway);
+        return jsonimg;
     }
 }
